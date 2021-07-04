9b1fc901e28f2233fdd344642b32a2ec59e44950
YassaFareed <yassa.fareed99@gmail.com>
2021-07-04 16:55:34 +0500

    integration testing done & mongodb connection & schema, rep

---
+++ test/auth/data/repository/AuthRepository.spec.ts
@@ -0,0 +1,59 @@
+ 
+ //integration testing (normally slow so it is not recommended to develop a bunch of these tests)
+ import { expect } from 'chai'
+ import dotenv from 'dotenv'
+ import { beforeEach } from 'mocha'
+ import mongoose from 'mongoose'
+ import AuthRepository from '../../../../src/auth/data/repository/AuthRepository'
+ 
+ dotenv.config()
+ 
+ describe('AuthRepository', () => {
+   let client: mongoose.Mongoose
+   let sut: AuthRepository
+ 
+   beforeEach(() => {
+     client = new mongoose.Mongoose()
+     const connectionStr = encodeURI(process.env.TEST_DB as string)
+     client.connect(connectionStr, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     })
+ 
+     sut = new AuthRepository(client)
+   })
+ 
+   afterEach(() => {
+     client.disconnect()
+   })
+
+
+   it('should return user when email is found', async () => {
+    //arrange
+    const email = 'mail@mail.com'
+
+    //act
+    const result = await sut.find(email)
+    //assert
+    expect(result).to.not.be.empty
+  })
+
+ 
+   it('should return user id when added to db', async () => {
+     //arrange
+     const user = {
+       name: 'John Flyn',
+       email: 'lyn@mail.com',
+       password: 'pass232',
+       type: 'email',
+     }
+ 
+     //act
+     const result = await sut
+       .add(user.name, user.email, user.type, user.password)
+       .catch(() => null)
+     //assert
+     expect(result).to.not.be.empty
+   })
+ })
+
